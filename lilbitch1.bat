ce71aeeb42386ed1b27d641238b32cd00f0d7e37
xbl3 <58650872+xbl3@users.noreply.github.com>
2020-05-26 15:34:22 -0400

    fdshzgdfj

---
+++ lilbitch1.bat
@@ -0,0 +1,89 @@
+rem needs a lot of cleaning - just hate windows a lot lately..
+):%)(you really only need this fucking dick
+:%$":iveEscapedAllThatIcanSoIwonderWhatItllBitchAboutBesidesBeingAfuckface
+
+@echo off
+set "list=\tumblr_lazyangels-network.tumblr.com/tumblr_lazyangels-network.tumblr.com\urls.txt"
+
+setlocal enabledelayedexpansion
+:var
+for /f "useback delims=" %%i in ("%list%") do (echo:&echo:%%~nxi&echo:%%~i&call set "url=%%~i"&call:iCantHandleShitImLeaving "%%~i" love mccunty the twat bitch)
+goto:eof
+)(echo:%%~i&echo:^<a href^="%%~i"^>^<img src="%%~i"^>^</a^>)&echo:^<br^>
+
+set "g3=&for /l %%i in (1,1,3) do echo:&goto:eof"
+:absolute
+for /f "useback delims=" %%i in ("C:\to\tumblr_lazyangels-network.tumblr.com\tumblr_lazyangels-network.tumblr.com\urls.txt") do @(echo:&echo:%%~nxi&echo:%%~i&echo:^<a href^="%%~i"^>^<img src="%%~i"^>^</a^>)&echo:^<br^>
+
+
+:iCantHandleShitImLeaving
+echo%0{%*}
+set "input=%~1"
+echo:%URL%
+call:processMyLittleShitStainPlesase %0 "%~1"&goto:eof
+call:littleSlutsCheckIn %0 %*
+
+goto:eof
+:justCauseImSuchAlittleCuntInowHaveToPassitAgain
+call:littleSlutsCheckIn %0 %*
+echo:^<a href^="%~1"^>^<img src="%~1"^>^</a^>)&echo:^<br^>&goto:eof
+
+
+:justCauseWindersHasAbiggerTwatThanAKardashian
+call:littleSlutsCheckIn %0 %*
+call echo:^<a href^="%~1"^>^<img src="%~1"^>^</a^>)>>%0.twat1.log 2>>%0.twat2.log & echo:&echo:^<br^>>>%0.twat1b.log 2>>%0.twat2b.log & echo:&goto:eof
+
+:ohDidImeantionHesAfuckingLoser
+call:littleSlutsCheckIn %0 %*
+echo:^^<a href^^="%~1"^^>^^<img src="%~1"^^>^^</a^^>)>>%0.twat1.log 2>>%0.twat2.log & echo:^"^^<br^^>^">>%0.twat1a.log 2>>%0.twat2a.log&goto:eof
+:ohIWonderHowFuckerIsNotDoing
+call:littleSlutsCheckIn %0 %*
+echo:^<a href^=^"%~1^"^>^<img src="%~1"^>^<^/a^>) 2>>%0.twat1.log (echo:^<br^>) 2>>%0.twat2.log &goto:eof
+:iveEscapedAllThatIcanSoIwonderWhatItllBitchAboutBesidesBeingAfuckface
+call:littleSlutsCheckIn %0 %*
+::echo:^<a href^=^"%~1^"^>^<img src="%~1"^>^<^/a^>&echo:^<br^>
+echo:%NAME%>>iveEscaped.log
+echo:%URL%>>iveEscaped.log
+echo:^<a href^=^"%~1^"^>^<img src="%~1"^>^<^/a^>>>iveEscaped.log&echo:^<br^>>>iveEscaped.log
+::echo:(^<a href^=^"%~1^"^>^<img src="%~1"^>^<^/a^>
+::echo:^<br^>)>>iveEscapedAllThatIcanSoIwonderWhatItllBitchAboutBesidesBeingAfuckface.twat.log 2>>%0.twat.log
+goto:eof
+:::>>iveEscapedAllThatIcanSoIwonderWhatItllBitchAboutBesidesBeingAfuckface.twat.log 2>>%0.twat.log
+
+::type iveEscapedAllThatIcanSoIwonderWhatItllBitchAboutBesidesBeingAfuckface.twat.log
+::getList.bat
+
+:processMyLittleShitStainPlesase		iCantHandleShitImLeaving
+echo%0{%*}
+if "%~1" == ":iCantHandleShitImLeaving" (
+	echo:%2
+	echo:%URL%
+	echo:%INPUT%
+	
+	for %%T in (%%T) do (
+		echo:TWATS&for %%t in (
+
+"justCauseImSuchAlittleCuntInowHaveToPassitAgain"
+"ohDidImeantionHesAfuckingLoser"
+"ohIWonderHowFuckerIsNotDoing"
+"iveEscapedAllThatIcanSoIwonderWhatItllBitchAboutBesidesBeingAfuckface"
+"justCauseWindersHasAbiggerTwatThanAKardashian"
+
+) do (set /a littleBitchCount+=1&call:babyBitchBounce "%%~t" "%~2") ) )
+goto:eof
+
+:babyBitchBounce
+echo%0{%*}&echo:%littleBitchCount% %input% %URL%
+call:%~1 "%~2"&goto:eof
+
+:littleSlutsCheckIn
+echo/%0 
+call:gap3
+echo\%*
+call:littleSlutsCheckout & GOTO:EOF
+for %%i in (%*) do ECho:%%~i
+call:littleSlutsCheckout & GOTO:EOF
+:littleSlutsCheckout
+echo%0&goto:eof
+:gap3
+for /l %%i in (1,1,3) do echo:&goto:eof
